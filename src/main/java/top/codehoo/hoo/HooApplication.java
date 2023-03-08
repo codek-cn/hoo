b5773ba2669f02a938129bab64387b3cea876a39
殷绪凯 <1922565649@qq.com>
2023-03-08 11:26:15 +0800

    项目配置文件读取

--- src/main/java/top/codehoo/hoo/HooApplication.java
+++ src/main/java/top/codehoo/hoo/HooApplication.java
@@ -2,8 +2,11 @@ package top.codehoo.hoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import top.codehoo.hoo.config.projectProperties.GlobalProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GlobalProperties.class)
 public class HooApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/top/codehoo/hoo/config/projectProperties/GlobalProperties.java
@@ -0,0 +1,12 @@
+package top.codehoo.hoo.config.projectProperties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author : 殷绪凯
+ * @description : 项目全局配置
+ * @since : 2023年 03月 08日 11:23
+ */
+@ConfigurationProperties(prefix = "hoo")
+public class GlobalProperties {
+}
