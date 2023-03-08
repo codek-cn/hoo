245fcf850b7ffa0bc0d5a6ee3bde9a8bcd2e6fd0
殷绪凯 <1922565649@qq.com>
2023-03-08 10:21:47 +0800

    init

---
+++ src/main/java/top/codehoo/hoo/HooApplication.java
@@ -0,0 +1,13 @@
+package top.codehoo.hoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/codehoo/hoo/HooApplicationTests.java
@@ -0,0 +1,13 @@
+package top.codehoo.hoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
